05cf83c67b00120cb487ef77bbb570cb345db697
Bruno <brunodominguezbascones@yahoo.es>
2015-03-24 19:44:00 +0100

    Initial Commit

---
+++ HackerBooks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HackerBooks
+//
+//  Created by Bruno Domínguez on 24/03/15.
+//  Copyright (c) 2015 brunodominguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
